da4b78b2708a9b1ad1bf7da0c628692ca17c8efe
Sandor Weisz <sandor@mysteryleague.com>
2025-02-25 14:46:54 -0600

    Swap ladder back to left side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -162,7 +162,7 @@ function handleInput(event) {
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
 
       <!-- Clues -->
-      <div class="bg-white rounded-lg shadow p-6">
+      <div class="md:order-2 bg-white rounded-lg shadow p-6">
         <h2 class="text-xl font-bold mb-2">Transformations</h2>
         <p class="mb-4 text-sm">Each of the transformations below can be applied to one of the words in the ladder. It's up to you to figure out which goes with which.</p>
         <div class="space-y-3">
